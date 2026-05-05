1be54536e12df1d8ba71856722105634ac3674bf
Matheus Oliveira <mho.oliveira01@gmail.com>
2026-05-05 10:53:48 -0300

    removendo lixo do projeto

--- web-cade-meu-dinheiro-output/lib/finance.ts
+++
@@ -1,7 +0,0 @@
-export * from "./finance/types";
-export * from "./finance/categories";
-export * from "./finance/format";
-export * from "./finance/dates";
-export * from "./finance/id";
-export * from "./finance/balance";
-export * from "./finance/groupByCategory";
